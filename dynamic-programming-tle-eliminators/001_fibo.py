56807304572087c47852da34de1e5a1ee7c540da
omkarshirwadkar <omkarshirwadkar1693@gmail.com>
2026-07-20 23:53:00 +0530

    Fibonacci Modifications

--- dynamic-programming-tle-eliminators/001_fibo.py
+++ dynamic-programming-tle-eliminators/001_fibo.py
@@ -3,15 +3,11 @@ def fibRecurBrute(n):
         return 1
     return fibRecurBrute(n - 1) + fibRecurBrute(n - 2)
 
-
-
 def fibRecurDP(n, dp):
     if n <= 2:
         return 1
     dp[n] = fibRecurDP(n - 1, dp) + fibRecurDP(n - 2, dp)
     return dp[n]
-
-
 
 def fibIterDP(n):
     if n == 1:
@@ -21,7 +17,6 @@ def fibIterDP(n):
     for i in range(3, n + 1):
         dp[i] = dp[i - 1] + dp[i - 2]
     return dp[n]
-
 
 for i in range(int(input())):
     n = int(input())
